f5972445dc9e1318d81f9ffd61cb779c4861f923
Hid-K <61284529+Hid-K@users.noreply.github.com>
2020-03-19 01:23:53 +0300

    Update Neural.cpp

--- Neural.cpp
+++ Neural.cpp
@@ -1,92 +1,129 @@
 #include <iostream>
-#include <math.h>
+#include "HidNeural.cpp"
+#include <cstdlib>
 
-float LEARN_CO = 0.0f;
+float NNinput[9] =				  {0.0F, 0.0F, 0.0F,
+								   0.0F, 0.0F, 0.0F,
+								   0.0F, 0.0F, 0.0F};
 
-namespace NeuralNet
-{
-	const float e = 2.718281828459045f;
+float NNinput_correction[4][9]/* = {{1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f},
+								  
+								  {1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f},
+								  
+								  {1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f},
+								  
+								  {1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f,
+								   1.0f, 1.0f, 1.0f}}*/;
 
-	template<uintmax_t neuron_count>
-	class NeuralNetworkLayer
-	{
-	private:
-		float *  inputs;
-		float *  inputs_correction;
-		uintmax_t inputs_count;
-		uintmax_t neurons_count;
+float NNlay0_out[4] {0.0F, 0.0F, 0.0F, 0.0F};
 
-		float * outputs;
+float NNlay0_weights[3][4];
 
-	public:
-		NeuralNetworkLayer(float * input, float *input_weights, uintmax_t input_count,
-						   float * output)
-		{
-			this->inputs = 				input;
-			this->inputs_correction = 	input_weights;
-			this->inputs_count = 		input_count;
-			this->outputs = 			output;
-			this->neurons_count	=		neuron_count;
-		};
-		~NeuralNetworkLayer(){};
-		
-		void exec_data()
-		{
-			for (uintmax_t n = 0; n < this->neurons_count; ++n)
-			{
-				for (uintmax_t i = 0; i < this->inputs_count; ++i)
-				{
-					// std::cout<<*(inputs_correction + n*inputs_count + i);
-					this->outputs[n] = 0;
-					this->outputs[n] += this->inputs[i] * *(this->inputs_correction + n*this->inputs_count + i);
-				}
-				// std::cout<<std::endl;
-				this->outputs[n] = 1/(1+pow(e, this->outputs[n]));
-			}
-		};
 
-		void weights_correction(float k)
-		{
-			for (uintmax_t i = 0; i < this->inputs_count; ++i)
-			{
-				for (uintmax_t n = 0; n < this->neurons_count; ++n)
-				{
-					*(this->inputs_correction + n*this->inputs_count + i) = 
-					*(this->inputs_correction + n*this->inputs_count + i) + k * this->outputs[n]
-					* this->outputs[n] * (1-this->outputs[n]) * this->inputs[i];
-				}
-			}
-		}
-	};
-};
+float NNlay1_out[] {0.0F, 0.0F, 0.0F};
 
-float NNinput[2] =				  {0.0f, 0.0f};
-float NNinput_correction[2][2] = {{1.0f, 1.0f},
-							      {1.0f, 1.0f}};
+float NNlay1_weights[2][3];
 
-float input_lay_out[2] =  	    {0.0f, 0.0f};
-float input_lay_weights[2][3] = {{1.0f, 1.0f, 1.0f},
-							   	 {1.0f, 1.0f, 1.0f}};
 
-float lay1out[3] =  	   {0.0f, 0.0f, 0.0f};
-float lay1weights[3][3] = {{1.0f, 1.0f, 1.0f},
-						   {1.0f, 1.0f, 1.0f},
-						   {1.0f, 1.0f, 1.0f}};
+float NNoutput[2] = {0.0F, 0.0F};		//1 - vertical
+										//2 - horizontal
 
-float lay2out[3] =  	   {0.0f, 0.0f, 0.0f};
-float lay2weights[3][3] = {{1.0f, 1.0f, 1.0f},
-						   {1.0f, 1.0f, 1.0f},
-						   {1.0f, 1.0f, 1.0f}};
+float ideat_out[14][2] {{0.0F, 0.0F},				//0 0
 
-float NNoutput[1] = {0.0f};
+					    {1.0F, 0.0F},				//1 0
 
-// NeuralNet::NeuralNetworkLayer<2> NNinput1(NNinput, (float*)NNinput_correction, 2, input_lay_out);
+					    {0.0F, 1.0F},				//0 1
+					    
+					    {0.0F, 1.0F},				//0 1
 
-// NeuralNet::NeuralNetworkLayer<3> NNlay1(input_lay_out, (float*)input_lay_weights, 2, lay1out);
+						{0.0F, 1.0F},				//0 1
+						
+						{1.0F, 0.0F},				//1 0
+						
+						{1.0F, 0.0F},				//1 0
+						
+						{1.0F, 1.0F},				//1 1
+						
+						{1.0F, 1.0F},				//1 1
 
-NeuralNet::NeuralNetworkLayer<2> NNlay2(NNinput, (float*)NNinput_correction, 2, lay2out);
+						{1.0F, 1.0F},				//1 1
 
-NeuralNet::NeuralNetworkLayer<1> NNoutput1(lay2out, (float*)lay2weights, 2, NNoutput);
+						{1.0F, 1.0F},				//1 1
+
+						{1.0F, 1.0F},				//1 1
+
+						{1.0F, 1.0F},				//1 1
+
+						{1.0F, 1.0F}};				//1 1
+
+float input_options[14][9] {{0.0f, 0.0f, 0.0f,
+							 0.0f, 0.0f, 0.0f,				//0 0
+							 0.0f, 0.0f, 0.0f},
+							
+							{1.0f, 0.0f, 0.0f,
+							 1.0f, 0.0f, 0.0f,				//1 0
+							 1.0f, 0.0f, 0.0f},
+							
+							{1.0f, 1.0f, 1.0f,
+							 0.0f, 0.0f, 0.0f,				//0 1
+							 0.0f, 0.0f, 0.0f},
+							
+							{0.0f, 0.0f, 0.0f,
+							 1.0f, 1.0f, 1.0f,				//0 1
+							 0.0f, 0.0f, 0.0f},
+							
+							{0.0f, 0.0f, 0.0f,
+							 0.0f, 0.0f, 0.0f,				//0 1
+							 1.0f, 1.0f, 1.0f},
+							
+							{0.0f, 1.0f, 0.0f,
+							 0.0f, 1.0f, 0.0f,				//1 0
+							 0.0f, 1.0f, 0.0f},
+							
+							{0.0f, 0.0f, 1.0f,
+							 0.0f, 0.0f, 1.0f,				//1 0
+							 0.0f, 0.0f, 1.0f},
+							
+							{1.0f, 1.0f, 1.0f,
+							 1.0f, 0.0f, 0.0f,				//1 1
+							 1.0f, 0.0f, 0.0f},
+							
+							{0.0f, 1.0f, 0.0f,
+							 1.0f, 1.0f, 1.0f,				//1 1
+							 0.0f, 1.0f, 0.0f},
+							
+							{0.0f, 0.0f, 1.0f,
+							 1.0f, 1.0f, 1.0f,				//1 1
+							 0.0f, 0.0f, 1.0f},
+							
+							{1.0f, 1.0f, 1.0f,
+							 0.0f, 1.0f, 0.0f,				//1 1
+							 0.0f, 1.0f, 0.0f},
+							
+							{0.0f, 1.0f, 0.0f,
+							 0.0f, 1.0f, 0.0f,				//1 1
+							 1.0f, 1.0f, 1.0f},
+							
+							{0.0f, 0.0f, 1.0f,
+							 0.0f, 0.0f, 1.0f,				//1 1
+							 1.0f, 1.0f, 1.0f},
+							
+							{0.0f, 1.0f, 0.0f,
+							 0.0f, 1.0f, 0.0f,				//1 1
+							 1.0f, 1.0f, 1.0f}};
+
+NeuralNet::NeuralNetworkLayer<NeuralNet::sygmoid> NNlay0(NNinput, (float*)NNinput_correction, 9, NNlay0_out, 4);
+
+NeuralNet::NeuralNetworkLayer<NeuralNet::sygmoid> NNlay1(NNlay0_out, (float*)NNlay0_weights, 4, NNlay1_out, 3);
+
+NeuralNet::NeuralNetworkLayer<NeuralNet::sygmoid> NNout(NNlay1_out, (float*)NNlay1_weights, 3, NNoutput, 2);
 
 int main(int argc, char const *argv[])
 {
@@ -97,106 +134,61 @@ int main(int argc, char const *argv[])
 	if (argc > 0)
 	{
 		LEARN_CO = (float)argv[0][0];
-	}
-	for (;;)
+	};
+	int qwe = 0;
+	for(;;)
 	{
-		// printf("\033" "[2J");
-
-		NNinput[0] = 0.0f;
-		NNinput[1] = 0.0f;
-
-		// NNinput1.exec_data();
-		// NNlay1.exec_data();
-		NNlay2.exec_data();
-		NNoutput1.exec_data();
-
-		test0 = NNoutput[0];
-
-		std::cout<<NNoutput[0]<<std::endl;
-
-		NNinput[0] = 1.0f;
-		NNinput[1] = 0.0f;
-
-		// NNinput1.exec_data();
-		// NNlay1.exec_data();
-		NNlay2.exec_data();
-		NNoutput1.exec_data();
-
-		test1 = NNoutput[0];
-
-		std::cout<<NNoutput[0]<<std::endl;
-
-		NNinput[0] = 0.0f;
-		NNinput[1] = 1.0f;
-
-		// NNinput1.exec_data();
-		// NNlay1.exec_data();
-		NNlay2.exec_data();
-		NNoutput1.exec_data();
-
-		test2 = NNoutput[0];
-
-		std::cout<<NNoutput[0]<<std::endl;
-
-		NNinput[0] = 1.0f;
-		NNinput[1] = 1.0f;
-
-		// NNinput1.exec_data();
-		// NNlay1.exec_data();
-		NNlay2.exec_data();
-		NNoutput1.exec_data();
-
-		test3 = NNoutput[0];
-
-		std::cout<<NNoutput[0]<<std::endl;
-		std::cout<<std::endl;
-		if (test0 == 0.0f && test1 == 1.0f && test2 == 1.0f && test3 == 1.0f)	//If network learned correctly rinting all weights:
+		qwe ++;
+		bool tests_result[14]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		for (int v = 0; v < 14; ++v)
 		{
-			std::cout<<std::endl;
-			std::cout<<"Input lay weights:"<<std::endl;
-			for (int n = 0; n < sizeof(input_lay_weights)/sizeof(*input_lay_weights); ++n)
+			for (uintmax_t i = 0; i < 9; ++i)
 			{
-				for (int i = 0; i < sizeof(input_lay_weights[0])/sizeof(*input_lay_weights[0]); ++i)
-				{
-					std::cout<<input_lay_weights[n][i];
-				}
-				std::cout<<std::endl;
-			}
+				NNinput[i] = input_options[v][i];
+			};
 
-			std::cout<<std::endl;
-			std::cout<<"Lay 1 weights:"<<std::endl;
-			for (int n = 0; n < sizeof(lay1weights)/sizeof(*lay1weights); ++n)
+			NNlay0.exec_data();
+			NNlay1.exec_data();
+			NNout.exec_data();
+
+			std::cout<<NNoutput[0]<<" "<<NNoutput[1]<<std::endl;
+
+			if (NNoutput[0] == ideat_out[v][0] && NNoutput[1] == ideat_out[v][1])
 			{
-				for (int i = 0; i < sizeof(lay1weights[0])/sizeof(*lay1weights[0]); ++i)
-				{
-					std::cout<<lay1weights[n][i];
-				}
-				std::cout<<std::endl;
-			}
-
-			std::cout<<std::endl;
-			std::cout<<"Lay 2 weights:"<<std::endl;
-			for (int n = 0; n < sizeof(lay2weights)/sizeof(*lay2weights); ++n)
+				tests_result[v]=1;
+				continue;
+			}else
 			{
-				for (int i = 0; i < sizeof(lay2weights[0])/sizeof(*lay2weights[0]); ++i)
-				{
-					std::cout<<lay2weights[n][i];
-				}
-				std::cout<<std::endl;
-			}
+				float input_error[9]{0.0f, 0.0f, 0.0f,
+									 0.0f, 0.0f, 0.0f,
+									 0.0f, 0.0f, 0.0f};
 
-			break;
-		} else		//If network learned wrong trying to fing error and correct weights
-		{
-			// NNinput1.weights_correction(LEARN_CO);
-			// NNlay1.weights_correction(LEARN_CO);
-			NNlay2.weights_correction(LEARN_CO);
-			NNoutput1.weights_correction(LEARN_CO);
+				float output_error[2]{ideat_out[v][0] - NNoutput[0], ideat_out[v][0] - NNoutput[0]};
+
+				float NNlay1_error[3]{0.0, 0.0, 0.0};
+
+				float NNlay0_error[4]{0.0, 0.0, 0.0, 0.0};
+
+				NNlay1.find_error(output_error);
+
+				NNlay0.find_error(NNlay1_error);
+
+				NNlay0.weights_correction(0.1, NNlay1_error);
+				NNlay1.weights_correction(0.1, output_error);
+			};
 		};
-		if (test0 < 0 || test1 < 0 || test2 < 0 || test3 < 0)
+		if (tests_result[0] 	&& tests_result[1] 		&&
+		 	tests_result[2] 	&& tests_result[3] 		&&
+		 	tests_result[4] 	&& tests_result[5] 		&& 
+		 	tests_result[6] 	&& tests_result[7] 		&& 
+		 	tests_result[8] 	&& tests_result[9] 		&& 
+		 	tests_result[10] 	&& tests_result[11] 	&& 
+			tests_result[12] 	&& tests_result[13])
 		{
-			return 0;
+			break;
 		}
+		if(qwe > 154){break;};
 	}
+
 	return 0;
 };
